7f673232d97afb52cae98bd06a09a9497aa40039
Thomas Ricouard <ricouard77@gmail.com>
2019-06-05 09:02:39 -0700

    WIP edit form

--- SwiftUIDemo/SwiftUIDemo/views/UserEditForm.swift
+++ SwiftUIDemo/SwiftUIDemo/views/UserEditForm.swift
@@ -1,29 +1,54 @@
  //
-//  UserEditForm.swift
-//  SwiftUIDemo
-//
-//  Created by Thomas Ricouard on 05/06/2019.
-//  Copyright © 2019 Thomas Ricouarf. All rights reserved.
-//
-
-import SwiftUI
-
-struct UserEditForm : View {
+ //  UserEditForm.swift
+ //  SwiftUIDemo
+ //
+ //  Created by Thomas Ricouard on 05/06/2019.
+ //  Copyright © 2019 Thomas Ricouarf. All rights reserved.
+ //
+ 
+ import SwiftUI
+ 
+ struct UserEditForm : View {
     var user: User
+    
+    @State var newUserName = ""
+    @State var newUserUsername = ""
     
     var body: some View {
         NavigationView {
-            Text(user.name)
-                .navigationBarItems(trailing: EditButton())
-                .navigationBarTitle(Text("Edit \(user.name)"))
+            VStack(alignment: .leading, spacing: 10) {
+                Text("User name")
+                TextField($newUserName)
+                    .textFieldStyle(.roundedBorder)
+                Divider()
+                Text("Username")
+                TextField($newUserUsername)
+                    .textFieldStyle(.roundedBorder)
+                }.padding(16)
+                Button(action: save) {
+                    Text("Save")
+                }
+                .navigationBarItems(trailing: Button(action: close) {
+                    Text("Close")
+                })
+                .navigationBarTitle(Text("Edit \(user.name)"), displayMode: .inline)
         }
     }
-}
-
-#if DEBUG
-struct UserEditForm_Previews : PreviewProvider {
+    
+    func save() {
+        
+    }
+    
+    func close() {
+        
+    }
+ }
+ 
+ #if DEBUG
+ struct UserEditForm_Previews : PreviewProvider {
     static var previews: some View {
         UserEditForm(user: sampleData[0])
     }
-}
-#endif
+ }
+ #endif
+
